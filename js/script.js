4b743587524b0645c026027b223b8636007ca128
yoshiimdg211 <152764341+yoshiimdg211@users.noreply.github.com>
2023-12-03 13:48:47 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        920:{
+            slidesPerView: 3,
+        },
+    },
+  });
