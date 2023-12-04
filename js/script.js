404a2e0c5929fedea0b957b6bb0e37331fb51837
yoshiimdg211 <152764341+yoshiimdg211@users.noreply.github.com>
2023-12-04 10:28:52 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
     centerSlide: 'true',
+    loop: true,
     fade: 'true',
     pagination: {
       el: ".swiper-pagination",
